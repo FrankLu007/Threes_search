516dc22a00e23fbe1d7fd4dc25c7f662708955e8
Frank <shes990106@gmail.com>
2018-11-26 10:55:40 +0800

    v5

--- Threes.cpp
+++ Threes.cpp
@@ -1,13 +1,15 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <unordered_map>
+#include <vector>
+#include <cmath>
 class Board
 {
 public:
 	int board[6];
 	Board() : board({0 ,0 ,0 ,0 ,0 ,0}) {}
-	int getindex(int next_tile, int dir = 4)
+	int getindex(const int next_tile, const int dir = 4)
 	{
-		int index = 0;
-		index = dir;
+		int index = dir;
 		for(int i = 0 ; i < 6 ; i++)
 		{
 			index <<= 4;
@@ -32,7 +34,7 @@ public:
 	bool slide_left()
 	{
 		bool move = 0;
-		for(int i = 2 ; i < 6 ; i++)
+		for(int i = 1 ; i < 6 ; i++)
 		{
 			if(!board[i] || i == 3) continue;
 			if(!board[i-1]) {board[i-1] = board[i]; board[i] = 0; move = 1;}
@@ -81,34 +83,32 @@ public:
 	State() {}
 	State(int a, float b, int c) : min(a), avg(b), max(c) {}
 };
-std::map <int, State> state;
+std::unordered_map <int, State> state;
 void P(Board board)
 {
 	printf("%d %d %d\n%d %d %d\n\n", board.board[0], board.board[1], board.board[2], board.board[3], board.board[4], board.board[5]);
 }
-int before_state(Board &board, bool bag[3], int next_tile);
-int test(Board &board, int next_tile , int dir = 4)
+int before_state(Board &board, bool bag[3], const int next_tile);
+int test(Board board, const int next_tile , int dir = 4)
 {
-	Board tmp = board;
 	if(state.find(board.getindex(next_tile, dir)) != state.end()) return board.getindex(next_tile, dir);
-	tmp.reflect();
+	board.reflect();
 	if(dir == 1 || dir == 3) dir = 4 - dir;
-	if(state.find(tmp.getindex(next_tile, dir)) != state.end()) return tmp.getindex(next_tile, dir);
-	tmp.reverse();
+	if(state.find(board.getindex(next_tile, dir)) != state.end()) return board.getindex(next_tile, dir);
+	board.reverse();
 	if(!dir || dir == 2) dir = 2 - dir;
-	if(state.find(tmp.getindex(next_tile, dir)) != state.end()) return tmp.getindex(next_tile, dir);
-	tmp.reflect();
+	if(state.find(board.getindex(next_tile, dir)) != state.end()) return board.getindex(next_tile, dir);
+	board.reflect();
 	if(dir == 1 || dir == 3) dir = 4 - dir;
-	if(state.find(tmp.getindex(next_tile, dir)) != state.end()) return tmp.getindex(next_tile, dir);
+	if(state.find(board.getindex(next_tile, dir)) != state.end()) return board.getindex(next_tile, dir);
 	return -1;
 }
-int after_state(Board &board, int dir, int next_tile, bool bag[3])
+int after_state(Board &board, const int dir, const int next_tile, bool bag[3])
 {
 	//printf("a\n");
 	//P(board);
 	int index = test(board, next_tile, dir);
 	if(index != -1) return index;
-	//if(state.find(board.getindex(next_tile, dir)) != state.end()) return board.getindex(next_tile, dir);
 	Board tmp = board;
 	int count = 0, min, max;
 	float sum = 0;
@@ -143,35 +143,27 @@ int after_state(Board &board, int dir, int next_tile, bool bag[3])
 	state[index] = State(min, sum / count, max);
 	return index;
 }
-int before_state(Board &board, bool bag[3], int next_tile)
+int before_state(Board &board, bool bag[3], const int next_tile)
 {
 	//printf("b\n");
 	//P(board);
 	int index = test(board, next_tile), best = -1;
 	if(index != -1) return index;
-	//if(state.find(board.getindex(next_tile)) != state.end()) return board.getindex(next_tile);
 	Board tmp;
 	float avg = 0;
 	int count = 0, min, max;
-	min = 0;
-	max = 0;
 	for(int i = 0 ; i < 4 ; i++)
 	{
 		tmp = board;
 		if(!tmp.slide(i)) continue;
 		index = after_state(tmp, i, next_tile, bag);
-		//max = std::max(max, state[index].max);
-		//min = std::max(min, state[index].min);
 		if(best == -1 || state[index].avg > state[best].avg) best = index;
-		//avg = std::max(avg, state[index].avg);
 		count ++;
 	}
 	if(!count)
 	{
 		for(int i = 0 ; i < 6 ; i++) if(board.board[i] > 2) count += pow(3, board.board[i]-2);
-		max = min = count;
-		avg = count;
-		//return 1/0;
+		avg = max = min = count;
 	}
 	else
 	{
@@ -188,6 +180,8 @@ int main()
 	Board board;
 	state.clear();
 	bool bag[3] = {1, 1, 1};
+	char c;
+	int next_tile, index;
 	for(int i = 0 ; i < 6 ; i++)
 	{
 		for(int j = 0 ; j < 3 ; j++)
@@ -199,14 +193,12 @@ int main()
 			board.board[i] = 0;
 		}
 	}
-	char c;
-	int next_tile;
 	while(scanf(" %c", &c) != EOF)
 	{
 		for(int i = 0 ; i < 6 ; i++) scanf("%d", &board.board[i]);
 		scanf(" +%d", &next_tile);
 		printf("%c ", c);
-		for(int i = 0 ; i < 6 ; i++) printf("%d ", board.board[i]);
+		for(int i = 0 ; i < 6 ; i++)  printf("%d ", board.board[i]);
 		printf("+%d = ", next_tile);
 		for(int i = 0 ; i < 6 ; i++)
 		{
@@ -216,7 +208,6 @@ int main()
 		if(c == 'a')
 		{
 			bool y = 1;
-			int index;
 			for(int i = 0 ; i < 4 ; i++)
 			{
 				index = test(board, next_tile, i);
@@ -231,7 +222,7 @@ int main()
 		}
 		else
 		{
-			int index = test(board, next_tile);
+			index = test(board, next_tile);
 			if(state.find(index) != state.end()) printf("%d %f %d\n", state[index].min, state[index].avg, state[index].max);
 			else printf("-1\n");
 		}
